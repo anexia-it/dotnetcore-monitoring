80a099b6ecad2717c90616141fb6f601de7d408a
sume2000 <s_meier@gmx.at>
2021-10-08 17:13:54 +0200

    SIANXKE-35 set correct value for newest framework version

---
+++ VersionMonitorNetCore/Models/ReleasesJson.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Anexia.Monitoring.Models
+{
+    /// <summary>
+    ///     Dto for a single json array object returned by netcore release page
+    /// </summary>
+    internal class ReleasesJson
+    {
+        /// <summary>
+        ///     Gets or sets the release's main version number
+        /// </summary>
+        [JsonProperty(PropertyName = "channel-version")]
+        public string MainVersion { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the release's product name
+        /// </summary>
+        [JsonProperty(PropertyName = "product")]
+        public string Product { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the release's date
+        /// </summary>
+        [JsonProperty(PropertyName = "latest-release-date")]
+        public DateTime LatestReleaseDate { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the release's support phase
+        /// </summary>
+        [JsonProperty(PropertyName = "support-phase")]
+        public string SupportPhase { get; set; }
+    }
+}
